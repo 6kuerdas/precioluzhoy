8d832ceb940b773ea795490ba0ddbf8dc580388f
6kuerdas <6kuerdas@gmail.com>
2025-03-25 17:24:13 +0100

    gitignore

--- rxconfig.py
+++ rxconfig.py
@@ -4,8 +4,9 @@ config = rx.Config(
     app_name="precioluzhoy",
     
     
-    cors_allowed_origins = ["https://precioluzhoy-nine.vercel.app",
+    cors_allowed_origins = [
                             "https://precioluzhoy-production.up.railway.app",
+                              "https://precioluzhoy-nine.vercel.app",
                             
                             "http://localhost:3000"]
                     )
