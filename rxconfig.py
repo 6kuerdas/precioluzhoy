fdcfab6f647079fb493dd0f0600423f073988417
6kuerdas <6kuerdas@gmail.com>
2025-03-25 16:24:06 +0100

    cors 8000

--- rxconfig.py
+++ rxconfig.py
@@ -3,9 +3,10 @@ import reflex as rx
 config = rx.Config(
     app_name="precioluzhoy",
     
-
+    api_url = "https://precioluzhoy-production.up.railway.app:8000",
+    
     cors_allowed_origins = ["https://precioluzhoy-nine.vercel.app",
                             "https://precioluzhoy-production.up.railway.app",
                             
                             "http://localhost:3000"]
-)
+                    )
