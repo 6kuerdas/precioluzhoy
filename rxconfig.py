ff5bdca3b81dbc1fc76c683a0efdf07937833143
6kuerdas <6kuerdas@gmail.com>
2025-03-25 16:07:00 +0100

    cors

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="precioluzhoy",
-    db_url="sqlite:///reflex.db",
+    
 
     cors_allowed_origins = ["https://precioluzhoy-nine.vercel.app/",
                             "https://precioluzhoy-production.up.railway.app",
