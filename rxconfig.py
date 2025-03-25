38f71e4fd192c461573765e75859df0b5a843c72
6kuerdas <6kuerdas@gmail.com>
2025-03-25 17:17:33 +0100

    domain

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,6 @@ import reflex as rx
 config = rx.Config(
     app_name="precioluzhoy",
     
-    api_url = "https://precioluzhoy-production.up.railway.app:8000",
     
     cors_allowed_origins = ["https://precioluzhoy-nine.vercel.app",
                             "https://precioluzhoy-production.up.railway.app",
