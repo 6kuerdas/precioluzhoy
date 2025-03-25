38c5b81cfe00a0ce5cb10603c2ae2d1ca17e2bf9
6kuerdas <6kuerdas@gmail.com>
2025-03-25 16:15:36 +0100

    cors trailing

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,7 @@ config = rx.Config(
     app_name="precioluzhoy",
     
 
-    cors_allowed_origins = ["https://precioluzhoy-nine.vercel.app/",
+    cors_allowed_origins = ["https://precioluzhoy-nine.vercel.app",
                             "https://precioluzhoy-production.up.railway.app",
                             
                             "http://localhost:3000"]
