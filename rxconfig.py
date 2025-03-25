f0a0ba2d1e65cb5f4dfd6f9167ae3e6ad54e8a30
6kuerdas <6kuerdas@gmail.com>
2025-03-26 00:44:23 +0100

    ...

--- rxconfig.py
+++ rxconfig.py
@@ -5,7 +5,7 @@ config = rx.Config(
     
     
     cors_allowed_origins = [
-                            "https://precioluzhoy.up.railway.app",
+                            "https://precioluzhoy-production-es.up.railway.app",
                               "https://precioluzhoy-es.vercel.app",
                              "http://localhost:3000",
                              "http://localhost:3001",
